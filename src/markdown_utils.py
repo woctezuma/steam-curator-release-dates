29e58048186e45c3d57b15dd4c51947a92574e05
Wok <woctezuma@users.noreply.github.com>
2022-11-23 19:24:58 +0100

    Add Markdown utils

---
+++ src/markdown_utils.py
@@ -0,0 +1,31 @@
+HEADERS = ["AppID", "Title", "Hidden date", "Public date"]
+ENTRY_FIELDS = ["appid", "title", "rt_release_date", "release"]
+PLACE_HOLDER = "---"
+
+
+def to_table_row(row_no, str_elements):
+    concatenated_elements = "|".join(str_elements)
+    line = f"|{row_no}|{concatenated_elements}|"
+    return line
+
+
+def get_headers_line():
+    place_holder_for_number = "#"
+    return to_table_row(place_holder_for_number, HEADERS)
+
+
+def get_separator_line():
+    place_holder = PLACE_HOLDER
+    num_headers = len(HEADERS)
+    return to_table_row(place_holder, [place_holder] * num_headers)
+
+
+def format_data_as_markdown(data):
+    lines = [get_headers_line(), get_separator_line()]
+
+    for i, entry in enumerate(data, start=1):
+        line = to_table_row(i, [str(entry[k]) for k in ENTRY_FIELDS])
+
+        lines.append(line)
+
+    return lines
